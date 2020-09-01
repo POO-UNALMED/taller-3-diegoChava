bb1507f167855bbf0560f103465a29cfb31531ad
diegoChava <69688169+diegoChava@users.noreply.github.com>
2020-09-01 16:24:27 -0500

    Taller3_p1

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,15 @@
+package taller3.televisores;
+
+public class Marca {
+	String nombre;
+	
+	public Marca(String str) {
+		nombre=str;
+	}
+	public void setNombre(String s) {
+		nombre=s;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+}
---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,116 @@
+package taller3.televisores;
+
+public class TV {
+	Marca marca;
+	int precio=500;
+	int canal=1;
+	int volumen=1;
+	Control control;
+	boolean estado;
+	static int numTV;
+	public TV(Marca mar, boolean b) {
+		marca=mar;
+		estado=b;
+		numTV++;
+	}
+	
+	public static void setNumTV(int num) {
+		numTV=num;
+	}
+
+	public void setPrecio(int p) {
+		precio=p;
+	}
+
+	public void setCanal(int c) {
+		if (estado) {
+			if (c>=1 && c<=120) {
+				canal=c;
+			}
+		}
+	}
+
+	public void setVolumen(int v) {
+		volumen=v;
+	}
+	
+	public void setControl(Control con) {
+		control=con;
+	}
+	
+	public void setMarca(Marca m) {
+		marca=m;
+	}
+
+	public int getPrecio() {
+		return precio;
+	}
+
+	public int getCanal() {
+		return canal;
+	}
+	
+	public int getVolumen() {
+		return volumen;
+	}
+	
+	public Control getControl() {
+		return control;
+	}
+
+	public Marca getMarca() {
+		return marca;
+	}
+	
+	public static int getNumTV() {
+		return numTV;
+	}
+	
+	public void turnOn() {
+		estado=true;
+	}
+	
+	public void turnOff() {
+		estado=false;
+	}
+	
+	public void volumenUp() {
+		if (estado) {
+			if(volumen<6 && volumen>-1) {
+				volumen++;
+			}
+		}
+	}
+	
+	public void volumenDown() {
+		if (estado) {
+			if(volumen<7 && volumen>0) {
+				volumen--;
+			}
+		}
+	}
+	
+	public void canalUp() {
+		if (estado) {
+			if(canal<120 && canal>0) {
+				canal++;
+			}
+		}
+	}
+	
+	public void canalDown() {
+		if (estado) {
+			if(canal<121 && canal>1) {
+				canal--;
+			}
+		}
+	}
+	public boolean getEstado() {
+		return estado;
+	}
+	
+	
+	
+
+
+}
